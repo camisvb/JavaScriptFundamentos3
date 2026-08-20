8deaa2440a005a50557ebf2a80be1119e128f57e
CAMILY VITÓRIA FEITOSA BISPO <26171643@edu119.local>
2026-08-20 16:56:16 -0300

    Salvando meu projeto

---
+++ 07-poo.js/02-get-set.js
@@ -0,0 +1,19 @@
+class Pessoa{
+    #nome;
+
+    constructor(nome){
+        this.#nome = nome
+    }
+
+    get nome(){
+        return this.#nome
+    }
+
+    set nome(valor){
+        if(valor.lenght >= 2) this.#nome = valor;
+    }
+}
+let pessoa = new Pessoa("Pedro");
+console.log(pessoa.nome);
+pessoa.nome = "Igor";
+console.log(pessoa.nome);
